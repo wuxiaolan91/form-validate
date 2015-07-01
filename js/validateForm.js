f2939b68505946bc402e7f287ca77cf5b0b0819f
吴晓兰 <wuxiaolan@wecash.net>
2015-07-01 20:28:11 +0800

    挪进来了几个表单验证的方法.

--- js/validateForm.js
+++ js/validateForm.js
@@ -1,8 +1,43 @@
 (function(){
-	var CFG = null;
-	var validateForm = {
-		
-	}
+	/**
+	 * auther:吴晓兰
+	 * 这个表单验证组件,我打算写成一个zepto的元素的拓展方法
+	 */
+	var cfg = null;
+	
+	var validateTip = "";
+	var validateObj = {
+		isBankNumber:function(str){
+			if(str.length==16||str.length==19){
+				return true;
+			}else{
+				return false;
+			}
+		},
+		/**
+		 * 验证是不是中文字符
+		 * @param  {[type]} strChinese [description]
+		 * @param  {[type]} showMsg    [description]
+		 * @return {[type]}            [description]
+		 */
+		isChinese:function(strChinese,showMsg){
+			var reg=/^[\u4e00-\u9fa5]+$/i;
+		   //var reg=/^[\u4E00-\u9FA5]{2,5}(?:·[\u4E00-\u9FA5]{2,5})*/;//支持少数民族
+		   var str=$.trim(strChinese);
+		   if(!reg.test(str)){
+		       //if(showMsg==null||showMsg=="") alert("请输入汉字!");
+		       return false;
+		   }   
+		   return true;
+		}
+
+	};
+	validateObj
+	
+	
+	//start
+	
+	//end
 	/**
 	 * [checkRule 对表单元素的语法检测]
 	 * @param  {dom结构} elem [要检测语法的js dom节点]
@@ -16,33 +51,31 @@
 		//this = elem;
 			var thisSelect = null,
 				$parent = $(this).parent(),
-				$tip = $parent.children('.tip'),//这个输入项的错误提示元素
-				validateRule = $(this).data("validate"),//这里面不同的验证规则应该是用逗号分隔的
-				isVisible = $(this).is(":visible"),//如果页面有用户隐藏的表单元素,那么不对它进行表单校验.
-				validateTip = $(this).data("vtip"),
+				$tip = $parent.children('.tip'),// 这个输入项的错误提示元素
+				validateRule = $(this).data("validate"),// 这里面不同的验证规则应该是用逗号分隔的
+				isVisible = $(this).is(":visible"),// 如果页面有用户隐藏的表单元素,那么不对它进行表单校验.
 				isValidatePass = true;
-			if(!validateRule || !isVisible){
-				return true;
-			}
-			var validateItemName = validateRule.split(":")[0],//验证项的名称
-				validateItemValue = validateRule.split(":")[1],
+
+			if(!validateRule || !isVisible) return false;
+			var validateArray = validateRule.split(":");
+			var validateItemName = validateArray[0],//验证项的名称
+				validateItemValue = validateArray[1],//验证项的规则
 				nodeName = $(this)[0].nodeName,
 				minLength = 0,
-				value = "", //填写项的内容
-				tipContent = "", //错误提示的内容
-				isThisValidate = true, //该输入项是否通过验证,默认为是
-				isNull = false; //当必填项用户没有填写时,此项为false.
-			if($tip.length<1){//如果该元素下没有错误提示元素，就创建一个
+				value = "", // 填写项的内容
+				tipContent = "", // 错误提示的内容
+				isThisValidate = true, // 该输入项是否通过验证,默认为是
+				isNull = false; // 当必填项用户没有填写时,此项为false.
+
+			if($tip.length<1){ // 如果该元素下没有错误提示元素，就创建一个
 				$tip = $("<p class='tip'><span></span></p>");
 				$parent.append($tip);
 			}
 			
 			if(validateRule){//该元素有验证的规则
-				
 				value = $.trim($(this).prop("value"));
-				if(!value){
-					value = $(this).val();
-				}
+				if(!value) value = $(this).val();
+				
 				if(this.nodeName=="SELECT"){
 					thisSelect = $(this).children('option:selected')[0];
 					if(thisSelect.hasAttribute("value")&&thisSelect.value!=-1){
@@ -55,23 +88,21 @@
 				//各种不同类型的验证
 				var validateList = validateItemValue.split(",");
 				for(var i =0;i<validateList.length;i++){
-					var validate = validateList[i];
-					var length = 0;
-					var maxLength = 0;
-					var firstWord = validate.substr(0,1);
-					var secondWord = validate.substr(1,validate.length);
-					var splitPosition = 0;
-					var secondWordFirst = "";
-					var secondWordLast = "";
-					var strType = "";
-					if($(this).attr("id") == "extractAmount"){
-						// debugger;
-					}
+					var validate = validateList[i],
+						length = 0,
+						maxLength = 0,
+						firstWord = validate.substr(0,1),
+						secondWord = validate.substr(1,validate.length),
+						splitPosition = 0,
+						secondWordFirst = "",
+						secondWordLast = "",
+						strType = "";
 
 					if(validate=="required"){
 
 						if(!$.trim(value)){ //该元素为空
 							isNull = true;
+
 							if(nodeName=="SELECT"){
 								tipContent = "请选择" + validateItemName;
 							}else{
@@ -80,17 +111,12 @@
 							
 							isValidatePass = false;
 							isThisValidate = false;
-							
 							console.log(elem.attr('data-empty-msg')||tipContent);
-							
 							return false;
 						}
 					}else{
-						if(!value){
-							break;
-						}
+						if(!value) break;
 					}
-					
 					
 					if(validate.indexOf("n")==0){
 						//debugger;
@@ -131,16 +157,16 @@
 						
 					
 					if(validate == "passwordOther"){ //密码验证
-						if(!self.isPasswordOther(value)){ //手机格式不正确
+						if(!validateObj.isPasswordOther(value)){ //手机格式不正确
 							isThisValidate = false;
 						}
 					}else if(validate.indexOf("password")>-1){
-						if(!self.isPassword(value)){
+						if(!validateObj.isPassword(value)){
 
 							isThisValidate = false;
 						}
 					}else if(validate.indexOf("phone")>-1){ //验证手机格式
-						if(!self.checkCellPhone(value)){ //手机格式不正确
+						if(!validateObj.checkCellPhone(value)){ //手机格式不正确
 							isThisValidate = false;
 						}
 					}else if(validate=="inviteNum"){ //判断是否是邀请码
@@ -149,7 +175,7 @@
 						if(value.length!=8 && value.length!=11){
 							isThisValidate = false;
 						}else if (value.length == 11) {
-							if(!self.checkCellPhone(value)){ //手机格式不正确
+							if(!validateObj.checkCellPhone(value)){ //手机格式不正确
 								isThisValidate = false;
 							}
 						}
@@ -159,13 +185,13 @@
 							 isThisValidate = false;
 						}*/
 					}else if(validate.indexOf("telarea")>-1){
-						if(!self.checkTelarea(value)){ //固定电话区号不正确
+						if(!validateObj.checkTelarea(value)){ //固定电话区号不正确
 							isThisValidate = false;
 
 							isTelarea=false;
 						}
 					}else if(validate.indexOf("telnum")>-1){
-						if(!self.checkTelnum(value)){ //固定电话号码不正确
+						if(!validateObj.checkTelnum(value)){ //固定电话号码不正确
 							isThisValidate = false;
 						}
 						if(!isTelarea){
@@ -176,37 +202,31 @@
 							isThisValidate = false;
 						}
 					}else if(validate.indexOf("qq")>-1){ //验证QQ
-						if(!self.checkQQ(value)){
+						if(!validateObj.checkQQ(value)){
 							isThisValidate = false;
 						}
 					}else if(validate.indexOf("cardId")>-1){ //验证身份证格式
 						
-						if(self.checkcardId(value)!=true){ 
+						if(validateObj.checkcardId(value)!=true){ 
 							isThisValidate = false;
 						}
 					}else if(validate.indexOf("chinese")>-1||validate.indexOf("zh")>-1){
-						if(!self.isChinese(value)){ //有非中文字符的东西
+						if(!validateObj.isChinese(value)){ //有非中文字符的东西
 							isThisValidate = false;
 						}
 					}else if(validate.indexOf("email")>-1){
-						if(!self.isEmail(value)){ //不是邮件
+						if(!validateObj.isEmail(value)){ //不是邮件
 							isThisValidate = false;
 						}
 					}else if(validate.indexOf("bankNumber")>-1){ //银行卡号
 						try{
-							if(!self.isBankNumber(value)){ //不是邮件
+							if(!validateObj.isBankNumber(value)){ //不是邮件
 								isThisValidate = false;
 							}
 						}catch(ex){
 							
 						}
 						
-					}else if(validate.indexOf("beiyinBankNumber")>-1){
-						if(value.length==16||value.length==19){ //不是邮件
-							
-						}else{
-							isThisValidate = false;
-						}
 					}
 					if(validate.indexOf("interger")>-1){
 						//不是整数
@@ -215,11 +235,11 @@
 						}
 					}
 					if(validate.indexOf("company")>-1){
-						if(!self.isCompanyName(value)){ //不是公司名称
+						if(!validateObj.isCompanyName(value)){ //不是公司名称
 							isThisValidate = false;
 						}
 					}else if(validate.indexOf("trueName")>-1){
-						if(!self.isUserName(value)){ //不是公司名称
+						if(!validateObj.isUserName(value)){ //不是公司名称
 							isThisValidate = false;
 						}
 					//增加数字范围判断
@@ -239,12 +259,12 @@
 					if(validate.toLowerCase().indexOf("length")>-1){
 						if(validate.indexOf("minLength")>-1){
 							minLength = validate.substr(validate.indexOf("h")+1);
-							if(self.strLen(value) < minLength){ //有非中文字符的东西
+							if(validateObj.strLen(value) < minLength){ //有非中文字符的东西
 								isThisValidate = false;
 							}
 						}else if(validate.indexOf("maxLength")>-1){
 							maxLength = validate.substr(validate.indexOf("h")+1);
-							if(self.strLen(value) > maxLength){ //有非中文字符的东西
+							if(validateObj.strLen(value) > maxLength){ //有非中文字符的东西
 								isThisValidate = false;
 							}
 						}else{
@@ -275,13 +295,14 @@
 				if(validateTip){
 					tipContent = validateItemName+validateTip;
 				}
+
 				//增加自定义msg浮框消息
-				loadingTip.show({
+				/*loadingTip.show({
 					content: elem.attr('data-msg')||tipContent
-				});
+				});*/
 				//$tip.show().find("span").html(tipContent);
 			}
-			
+			console.log(tipContent);
 			return isValidatePass;
 
 		
@@ -337,7 +358,7 @@
 	$.fn.validateForm = function(paramData){
 		
 		var form = this;
-		CFG = $.extend(CFG,paramData);
+		cfg = $.extend(cfg,paramData);
 		var isValidatePass = true;//该表单是否验证通过，true为通过
 		var isTelarea = true;//固话区号是否通过验证,默认为是
 		//var $validateELemList = $(this).find("input,textarea,select:visible");
