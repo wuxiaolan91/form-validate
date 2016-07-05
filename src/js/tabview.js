80dae022474436105f97355baecb8e58493ee561
吴晓兰 <wuxiaolan@wecash.net>
2016-07-05 20:14:47 +0800

    修改. 仿照vue一样把所有源代码都放到src目录

---
+++ src/js/tabview.js
@@ -0,0 +1,8 @@
+define(["animate"],function(a){
+	function TabView(){
+		this.animate = new a.Animate();
+	}
+	return {TabView:TabView};
+	
+	
+});
---
+++ src/js/treeview.js
@@ -0,0 +1,4 @@
+define(function(){
+	function TreeView(){};
+	return {TreeView:TreeView};
+})
